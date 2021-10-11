164006834216a16676de3feaaee407ec35f54215
Liu Yahui <2113673297@qq.com>
2021-10-11 17:43:50 +0800

    Please enter the commit message for your changes. Lines starting  delete  :q

--- springboot0506/src/main/java/com/test/china/springboot0506/test/ApplicationContextDemo.java
+++
@@ -1,9 +0,0 @@
-package com.test.china.springboot0506.test;
-
-import org.springframework.context.ApplicationContext;
-
-public class ApplicationContextDemo {
-    public static void main(String[] args) {
-
-    }
-}
